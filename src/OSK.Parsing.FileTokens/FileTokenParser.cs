bb11af6bcb1b2ce611aeb1215c21dea7a396e62e
M <mmmasent@gmail.com>
2024-06-20 20:20:08 -0400

    Add static accessor for FileTokenReader (OSK-2)
    Motivation
    ----
    There is no way to access the internal implementation for the IFileTokenReader and thus this library is useless
    
    Modifications
    ----
    * Added simple static accessor that will create the FileTokenReader for consumers
    
    Result
    ----
    FileTokenReader is now usable

---
+++ src/OSK.Parsing.FileTokens/FileTokenParser.cs
@@ -0,0 +1,16 @@
+﻿using OSK.Parsing.FileTokens.Handlers;
+using OSK.Parsing.FileTokens.Internal.Services;
+using OSK.Parsing.FileTokens.Options;
+using OSK.Parsing.FileTokens.Ports;
+
+namespace OSK.Parsing.FileTokens
+{
+    public static class FileTokenParser
+    {
+        public static IFileTokenReader OpenRead(string filePath)
+            => OpenRead(filePath, DefaultTokenStateHandler.Instance);
+
+        public static IFileTokenReader OpenRead(string filePath, ITokenStateHandler tokenStateHandler)
+            => new FileTokenReader(filePath, tokenStateHandler, new FileTokenReaderOptions());
+    }
+}
